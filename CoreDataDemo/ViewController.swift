f735ed63fedb01e5ae0e740a9f0074f34cf13200
Raghavendra Dattawad <raghavendra.d@wickedride.com>
2018-06-11 03:52:12 -0700

    Core Data Demo swift Examples

--- CoreDataDemo/ViewController.swift
+++ CoreDataDemo/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+
+
+    @IBOutlet weak var userNameFiled: UITextField!
+    
+    
+    @IBOutlet weak var passwordField: UITextField!
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
+       
+     self.method()
+        
+
+        let myname =
+        self.method1(name: "einstein")
+    
+        print(myname)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +38,72 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func Save(_ sender: Any) {
+       
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let  context = appDelegate.persistentContainer.viewContext;
+        
+    
+        
+        let entity = NSEntityDescription.entity(forEntityName: "User", in: context)
+        let newUser = NSManagedObject(entity: entity!, insertInto: context)
+     
+        
+        newUser.setValue(self.userNameFiled.text, forKey: "userNme")
+        newUser.setValue(self.passwordField.text, forKey: "pwd")
+        
+        
+        
+        do{
+            try context.save()
+        }
+        catch{
+            
+            print("Failed to save")
+            
+            
+        }
+    
+    }
+    
+
+    @IBAction func Load(_ sender: Any) {
+    
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let  context = appDelegate.persistentContainer.viewContext;
+
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+        //request.predicate = NSPredicate(format: "age = %@", "12")
+        request.returnsObjectsAsFaults = false
+        do {
+            let result = try context.fetch(request)
+            for data in result as! [NSManagedObject] {
+                print(data.value(forKey: "userNme") as! String)
+            }
+
+        } catch {
+
+            print("Failed")
+        }
+
+
+
+    }
+    
+    
+    func method(){
+        
+       print("inside method")
+    }
+        
+    func method1(name:String) -> String {
+        
+        return name + "ns"
+    }
+        
+        
+        
+    
 }
 
